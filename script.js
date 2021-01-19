697c413759ba4a855d3c15fb875bfba102335886
CamilleTamoumi <37783597+CamilleTamoumi@users.noreply.github.com>
2021-01-19 14:37:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,40 @@
-/* exo 1 */
+var questions = [
+ 	{
+ 		question: 'Est-ce que tu aimes le JavaScript ?',
+ 		reponse : 'Oui'
+	 },
+	 {
+	 	question: 'Le JavaScript est une évolution du Java ?',
+		 reponse : 'Non'
+	 },
+	 {
+	 	question: 'Le JavaScript est proche du Java ?',
+	 	reponse : 'Non'
+	 },
+	 {
+	 	question: 'Le JavaScript est un langage d\'objet par prototypage ?',
+	 	reponse : 'Oui'
+	 },
+	 {
+		 question: 'Le JavaScript est souvent utilisé coté client ?',
+	 	reponse : 'Oui'
+	 },
+	 {
+		 question: 'Le JavaScript peut être utilisé coté client et serveur ?',
+		 reponse : 'Oui'
+	 }
+];
 
-var annee = prompt("entrez une annee");
+var reponse = [];
 
-if( (annee % 4 == 0 && annee % 100 > 0) || annee % 400 == 0) {
-	console.log("annee bissextile");
-} else {
-	console.log("annee non bissextile");
+for(var i = 0; i < questions.length; i++) {
+	reponse.push(prompt(questions[i].question));
 }
 
-/* exo 2 */
-var nombre = prompt("entrez un nombre");
-
-function estPremier(nombre) {
-	if(nombre <= 1) {
-		return false;
+for(var i = 0; i < questions.length; i++) {
+	if(questions[i].reponse.toLowerCase() == reponse[i].toLowerCase()) {
+		console.log("Bonne réponse pour la question " + questions[i].question);
+	} else {
+		console.log("Mauvaise réponse pour la question " + questions[i].question);
 	}
-
-	if(nombre == 2) {
-		return true;
-	}
-
-	for(var i = 2; i < nombre; i++) {
-		if(nombre % i == 0) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
-if(estPremier(nombre)) {
-	console.log("premier");
-} else {
-	console.log("non premier");
-}
-
-/* exo 3 */
-var semaine = ['lundi', 'mar', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanc'];
-
-semaine.pop();
-
-document.write("<div>" + semaine + "</div>");
-
-semaine.push('dimanche');
-semaine[1] = 'mardi';
-
-console.log(semaine.length);
-console.log(semaine[4]);
-
-document.write("<div>" + semaine + "</div>");
-
-/* exo 4 */
-var fruits = ['Banane', 'Raisin', 'Pomme'];
-
-console.log(fruits.join()); // join créé une chaine de caracteres
-console.log(fruits);
-
-var fruit = prompt("entrez un fruit");
-
-if(fruits.includes(fruit)) {
-	console.log(fruit);
-	fruits.splice(fruits.indexOf(fruit), 1);
-	console.log(fruits);
-} else {
-	console.log("indisponible");
 }
